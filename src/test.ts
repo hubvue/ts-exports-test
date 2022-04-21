14db772b080d6660c35161626e4e2bbe8126b8b3
hubvue <cckcc.kim@gmail.com>
2022-04-21 18:39:05 +0800

    test

---
+++ src/test.ts
@@ -0,0 +1,6 @@
+import { Type, Math } from '@cckim/ts-exports-test'
+import { type } from '@cckim/ts-exports-test/type'
+
+console.log(Type.type(123))
+console.log(Math.add(1, 2))
+console.log(type(2345))
