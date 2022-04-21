8ad46e1f5e868c1002f410c2caad1475bdbe8824
hubvue <cckcc.kim@gmail.com>
2022-04-21 18:50:40 +0800

    feat: test @cckim/types

--- src/test.ts
+++ src/test.ts
@@ -1,6 +1,8 @@
 import { Type, Math } from '@cckim/ts-exports-test'
 import { type } from '@cckim/ts-exports-test/type'
+import { getUA } from '@cckim/utils/env'
 
 console.log(Type.type(123))
 console.log(Math.add(1, 2))
 console.log(type(2345))
+console.log(getUA())
